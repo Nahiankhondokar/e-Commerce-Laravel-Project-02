832de9f1748f477a58cc8c448768a81ebb831033
Nahiankhondokar <nahiankhondokarphp@gmail.com>
2022-10-09 23:16:37 +0600

    all files images links load

--- resources/views/frontend/user_master.blade.php
+++ resources/views/frontend/user_master.blade.php
@@ -6,21 +6,21 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta name="description" content="">
 	<meta name="author" content="">
-	
+
 	<!-- Front style -->
-	<link id="callCss" rel="stylesheet" href="themes/css/front.min.css" media="screen"/>
-	<link href="themes/css/base.css" rel="stylesheet" media="screen"/>
+	<link id="callCss" rel="stylesheet" href="{{asset('')}}frontend/assets/css/front.min.css" media="screen"/>
+	<link href="{{asset('frontend/assets/css/base.css')}}" rel="stylesheet" media="screen"/>
 	<!-- Front style responsive -->
-	<link href="themes/css/front-responsive.min.css" rel="stylesheet"/>
-	<link href="themes/css/font-awesome.css" rel="stylesheet" type="text/css">
+	<link href="{{asset('frontend/assets/css/front-responsive.min.css')}}" rel="stylesheet"/>
+	<link href="{{asset('frontend/assets/css/font-awesome.css')}}" rel="stylesheet" type="text/css">
 	<!-- Google-code-prettify -->
-	<link href="themes/js/google-code-prettify/prettify.css" rel="stylesheet"/>
+	<link href="{{asset('frontend/assets/js/google-code-prettify/prettify.css')}}" rel="stylesheet"/>
 	<!-- fav and touch icons -->
-	<link rel="shortcut icon" href="themes/images/ico/favicon.ico">
-	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="themes/images/ico/apple-touch-icon-144-precomposed.png">
-	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="themes/images/ico/apple-touch-icon-114-precomposed.png">
-	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="themes/images/ico/apple-touch-icon-72-precomposed.png">
-	<link rel="apple-touch-icon-precomposed" href="themes/images/ico/apple-touch-icon-57-precomposed.png">
+	<link rel="shortcut icon" href="{{asset('frontend/assets/images/ico/favicon.ico')}}">
+	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="{{asset('frontend/assets/images/ico/apple-touch-icon-144-precomposed.png')}}">
+	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="{{asset('frontend/assets/images/ico/apple-touch-icon-114-precomposed.png')}}">
+	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="{{asset('frontend/assets/images/ico/apple-touch-icon-72-precomposed.png')}}">
+	<link rel="apple-touch-icon-precomposed" href="{{asset('frontend/assets/images/ico/apple-touch-icon-57-precomposed.png')}}">
 	<style type="text/css" id="enject"></style>
 </head>
 <body>
@@ -35,7 +35,7 @@
 		<div class="carousel-inner">
 			<div class="item active">
 				<div class="container">
-					<a href="#"><img style="width:100%" src="themes/images/carousel/1.png" alt="special offers"/></a>
+					<a href="#"><img style="width:100%" src="{{asset('')}}frontend/assets/images/carousel/1.png" alt="special offers"/></a>
 					<div class="carousel-caption">
 						<h4>First Thumbnail label</h4>
 						<p>Banner text</p>
@@ -44,7 +44,7 @@
 			</div>
 			<div class="item">
 				<div class="container">
-					<a href="register.html"><img style="width:100%" src="themes/images/carousel/2.png" alt=""/></a>
+					<a href="register.html"><img style="width:100%" src="{{asset('')}}frontend/assets/images/carousel/2.png" alt=""/></a>
 					<div class="carousel-caption">
 						<h4>Second Thumbnail label</h4>
 						<p>Banner text</p>
@@ -53,7 +53,7 @@
 			</div>
 			<div class="item">
 				<div class="container">
-					<a href="register.html"><img src="themes/images/carousel/3.png" alt=""/></a>
+					<a href="register.html"><img src="{{asset('')}}frontend/assets/images/carousel/3.png" alt=""/></a>
 					<div class="carousel-caption">
 						<h4>Third Thumbnail label</h4>
 						<p>Banner text</p>
@@ -79,12 +79,12 @@
 <!-- Footer ================================================================== -->
     @include('frontend.body.footer')   
 <!-- Placed at the end of the document so the pages load faster ============================================= -->
-<script src="themes/js/jquery.js" type="text/javascript"></script>
-<script src="themes/js/front.min.js" type="text/javascript"></script>
-<script src="themes/js/google-code-prettify/prettify.js"></script>
+<script src="{{asset('frontend/assets/js/jquery.js')}}" type="text/javascript"></script>
+<script src="{{asset('frontend/assets/js/front.min.js')}}" type="text/javascript"></script>
+<script src="{{asset('frontend/assets/js/google-code-prettify/prettify.js')}}"></script>
 
-<script src="themes/js/front.js"></script>
-<script src="themes/js/jquery.lightbox-0.5.js"></script>
+<script src="{{asset('frontend/assets/js/front.js')}}"></script>
+<script src="{{asset('frontend/assets/js/jquery.lightbox-0.5.js')}}"></script>
 
 </body>
 </html>
