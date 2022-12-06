11e4e70fb959a24c260bcd2a33a11c7faf1a3d22
Nahiankhondokar <nahiankhondokarphp@gmail.com>
2022-12-06 14:39:57 +0600

    order exports system excel file downlaod part 02

--- app/Exports/orderExpport.php
+++ app/Exports/orderExpport.php
@@ -3,6 +3,7 @@
 namespace App\Exports;
 
 use App\Models\Order;
+use App\Models\OrderProduct;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 
@@ -14,12 +15,37 @@ class orderExpport implements WithHeadings,FromCollection
     public function collection()
     {
         // get all order data
-        $data = Order::select('id', 'name','phone', 'order_status', 'payment_method', 'grand_total') -> get();
-        return $data;
+        $orderData = Order::select('id', 'name','phone', 'order_status', 'payment_method', 'grand_total') -> get();
+
+        // order product details
+        foreach($orderData as $key => $item){
+            $orderProduct = OrderProduct::select('id', 'product_code', 'product_name') -> where('order_id', $item -> id) -> get();
+            // $orderItem = json_decode(json_encode($orderProduct));
+
+            // get product code
+            $product_codes = '';
+            $product_name = '';
+            foreach($orderProduct as $item){
+                $product_codes .= $item -> product_code.',';
+                $product_name .= $item -> product_name.',';
+            }
+
+            /**
+             *  create a key with value
+             *  bind this key with the main array
+             */
+            $orderData[$key]['product_codes'] = $product_codes;
+            $orderData[$key]['product_name'] = $product_name;
+            // $orderData = json_decode(json_encode($orderData));
+            // echo '<pre>'; print_r($orderData); die; 
+            
+        }
+        // echo '<pre>'; print_r($orderData); die; 
+        return $orderData;
     }
 
-    // heading
+    // heading for all the coloum
     public function headings(): array{
-        return ['Id', 'Name', 'Phone', 'Status', 'Payment', 'Total'];
+        return ['Id', 'Name', 'Phone', 'Status', 'Payment', 'Total', 'Product Code', 'Product Name'];
     }
 }
--- resources/views/backend/order/order_view.blade.php
+++ resources/views/backend/order/order_view.blade.php
@@ -8,7 +8,7 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1>All Orders</h1>
+            <h1>Inventory Feature</h1>
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
@@ -24,64 +24,70 @@
     <section class="content">
       <div class="container-fluid">
         <div class="row">
-          <a href="{{ route('order.report') }}" class="btn btn-primary float-right">Order Reports</a> 
-          <a href="{{ route('order.exports') }}" class="btn btn-light float-right mr-1">Export Order</a> 
-          <br><br>
-          <div class="col-md-12">
-            <table id="category" class="table table-striped table-bordered">
-                <thead>
-                    <tr>
-                    <th scope="col">Order ID</th>
-                    <th scope="col">Customer Name</th>
-                    <th scope="col">Customer Email</th>
-                    <th scope="col">Product Code</th>
-                    <th scope="col">Payment</th>
-                    <th scope="col">Grand Total</th>
-                    <th scope="col">Created</th>
-                    <th scope="col">Status</th>
-                    <th scope="col">Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-        
-                    @foreach($order as $item)
-                    <tr>
-                        <th scope="row">{{ $item['id'] }}</th>
-                        <td>
-                            {{ $item['name'] }}
-                        </td>
-                        <td>
-                            {{ $item['email'] }}
-                        </td>
-                        <td>
-                            @foreach($item['order_product'] as $product)
-                            {{$product['product_code'] }} 
-                            ({{$product['product_qty']}})<br>
-                            @endforeach
-                        </td>
-                        <td>{{ $item['payment_method'] }}</td>
-                        <td>${{ $item['grand_total'] }}</td>
-                        <td>{{ date('d-m-Y', strtotime($item['created_at'])) }}</td>
-                        <td>{{ $item['order_status'] }}</td>
-                        <td style="width: 12%">
-                            <a title="Order Details" href="{{route('admin.order.details', $item['id'])}}" class="btn btn-sm btn-info"><i class="fa fa-eye" aria-hidden="true"></i>
-                            </a>
-                            @if($item['order_status'] == 'Shipped' || $item['order_status'] == 'Deliverd')
-                            <a title="Invoice" href="{{route('order.invoice', $item['id'])}}" class="btn btn-sm btn-primary"><i class="fa fa-print" aria-hidden="true"></i>
-                            </a>
-                            @endif
-
-                            <a target="_blank" title="PDF" href="{{route('order.pdf', $item['id'])}}" class="btn btn-sm btn-warning"><i class="fa fa-file-pdf" aria-hidden="true"></i>
-                            </a>
-
-                        </td>
+          <div class="card">
+            <div class="card-header">
+              <h3>All Order Item</h3>
+              <a href="{{ route('order.report') }}" class="btn btn-primary float-right">Order Reports</a> 
+              <a href="{{ route('order.exports') }}" class="btn btn-light float-right mr-1">Export Order</a> 
+            </div>
+            <div class="card-body">
+              <div class="col-md-12">
+                <table id="category" class="table table-striped table-bordered">
+                    <thead>
+                        <tr>
+                        <th scope="col">Order ID</th>
+                        <th scope="col">Customer Name</th>
+                        <th scope="col">Customer Email</th>
+                        <th scope="col">Product Code</th>
+                        <th scope="col">Payment</th>
+                        <th scope="col">Grand Total</th>
+                        <th scope="col">Created</th>
+                        <th scope="col">Status</th>
+                        <th scope="col" width="15%">Action</th>
                         </tr>
-                    @endforeach
+                    </thead>
+                    <tbody>
+            
+                        @foreach($order as $item)
+                        <tr>
+                            <th scope="row">{{ $item['id'] }}</th>
+                            <td>
+                                {{ $item['name'] }}
+                            </td>
+                            <td>
+                                {{ $item['email'] }}
+                            </td>
+                            <td>
+                                @foreach($item['order_product'] as $product)
+                                {{$product['product_code'] }} 
+                                ({{$product['product_qty']}})<br>
+                                @endforeach
+                            </td>
+                            <td>{{ $item['payment_method'] }}</td>
+                            <td>${{ $item['grand_total'] }}</td>
+                            <td>{{ date('d-m-Y', strtotime($item['created_at'])) }}</td>
+                            <td>{{ $item['order_status'] }}</td>
+                            <td style="width: 12%">
+                                <a title="Order Details" href="{{route('admin.order.details', $item['id'])}}" class="btn btn-sm btn-info"><i class="fa fa-eye" aria-hidden="true"></i>
+                                </a>
+                                @if($item['order_status'] == 'Shipped' || $item['order_status'] == 'Deliverd')
+                                <a title="Invoice" href="{{route('order.invoice', $item['id'])}}" class="btn btn-sm btn-primary"><i class="fa fa-print" aria-hidden="true"></i>
+                                </a>
+                                @endif
+
+                                <a target="_blank" title="PDF" href="{{route('order.pdf', $item['id'])}}" class="btn btn-sm btn-warning"><i class="fa fa-file-pdf" aria-hidden="true"></i>
+                                </a>
+
+                            </td>
+                            </tr>
+                        @endforeach
+                        
+                    </tbody>
+                </table>
                     
-                </tbody>
-            </table>
-                
-            <!-- /.card -->
+                <!-- /.card -->
+              </div>
+            </div>
           </div>
           <!-- /.col -->
         </div>
